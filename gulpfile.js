1c46f42f5bc2890cb4082644aa5290e68f44dada
ShanthiAcharya <shanthi.s.acharya@gmail.com>
2016-02-22 09:42:42 -0800

    fixes

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var concat = require('gulp-concat');
 
-gulp.task('default',['copy-html','scripts'],function(){
+gulp.task('default',['copy-html','scripts','copy-css'],function(){
 
     console.log('Gulp is running correctly')
     gulp.watch('*.html', ['copy-html']);
@@ -23,10 +23,18 @@ gulp.task('copy-html', function() {
     gulp.src('./*.html')
         .pipe(gulp.dest('./dist'));
 });
+gulp.task('copy-css', function() {
+
+    console.log('Gulp is copying css')
+    gulp.src('css/**/*.css')
+        .pipe(gulp.dest('./dist/css'));
+});
+
 
 gulp.task('dist', [
     'copy-html',
-    'scripts'
+    'scripts',
+    'copy-css'
 
 ]);
 
